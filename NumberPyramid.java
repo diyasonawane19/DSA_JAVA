f6ee5a73df06662dbf901c58a8223664af97c0cd
diyasonawane19 <diyasonawane21@gmail.com>
2026-04-01 23:35:58 +0530

    Add NumberPyramid.java to print number pyramids
    Implement a program to print a number pyramid based on user input for rows.

---
+++ NumberPyramid.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class NumberPyramid {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows :");
+        int rows = sc.nextInt();
+
+        for(int i=1;i<=rows;i++){
+            //spaces
+            for(int j=1;j<=rows-i;j++){
+                System.out.print(" ");
+            }
+            //numbers
+            for(int j=1;j<=i;j++){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
