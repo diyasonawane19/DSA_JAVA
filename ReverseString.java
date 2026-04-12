977f30dc129a0df5bcbb9b01533d3fac24147ba8
diyasonawane19 <diyasonawane21@gmail.com>
2026-04-12 23:01:56 +0530

    Remove package declaration from ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class ReverseString {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the string: ");
+    String str = sc.nextLine();
+
+    StringBuilder sb = new StringBuilder(str);
+    for(int i =0; i<sb.length()/2;i++){
+        int front = i;
+        int back = sb.length() -1 - i;
+
+        char frontChar =  sb.charAt(front);
+        char backChar = sb.charAt(back);
+
+        sb.setCharAt(front, backChar);
+        sb.setCharAt(back, frontChar);
+
+    }
+    System.out.println(sb);
+}
+}
