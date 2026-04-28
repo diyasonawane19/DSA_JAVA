1b9f615f94d4e473c453c77181b5a11acc29ba2c
diyasonawane19 <diyasonawane21@gmail.com>
2026-04-28 23:47:35 +0530

    Create InsertionSort.java

---
+++ InsertionSort.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class InsertionSort {
+    public static void printArray(int arr[]){
+        for(int i =0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements: " );
+        int n = sc.nextInt();
+        int arr [] = new int[n];
+        System.out.println("Enter the elements: ");
+        for(int i=0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        //insertion sort
+
+        for(int i=1;i<arr.length ; i++){
+            int current = arr[i];
+            int j = i-1;
+            while(j>=0 && current < arr[j]){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = current;
+        }
+        printArray(arr);
+        sc.close();
+    }
+}
